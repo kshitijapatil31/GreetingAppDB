8eec13503d9b8439fc55a0a1676ab312db6dbbb2
Kshitija Patil <patilkshitija2@gmail.com>
2021-09-26 20:46:46 +0530

    [kshitija]:Ability for greetingg app to give greetiing message

--- src/main/java/com/example/greetingappdb/controller/GreetingController.java
+++ src/main/java/com/example/greetingappdb/controller/GreetingController.java
@@ -1,6 +1,8 @@
 package com.example.greetingappdb.controller;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.greetingappdb.model.Greeting;
@@ -19,6 +21,11 @@ public class GreetingController {
 		
 		return new Greeting(counter.incrementAndGet(),String.format(template, name));
 		
+	}
+	@RequestMapping(value= {"/query"},method=RequestMethod.GET)
+	public String SayHello(@RequestParam (value="name",defaultValue="world") String name,@RequestParam(value="fName") String fName,@RequestParam(value="lName")String lName) {
+		return new Greeting(counter.incrementAndGet(),String.format(template, name))+fName+" "+lName+"";
+	
 	}
 	
 }
