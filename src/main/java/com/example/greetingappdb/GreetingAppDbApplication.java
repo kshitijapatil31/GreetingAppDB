7455c68eb7b22ea43df406b78f0c50231e5a92a6
Kshitija Patil <patilkshitija2@gmail.com>
2021-09-26 19:18:39 +0530

    [kshitija]:initial setup is added

---
+++ src/main/java/com/example/greetingappdb/GreetingAppDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.greetingappdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingAppDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetingAppDbApplication.class, args);
+	}
+
+}
